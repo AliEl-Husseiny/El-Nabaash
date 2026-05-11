51061c16e0b237c10580381a6a25ea0eb64821f6
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-05-11 16:07:09 +0300

    Add Swagger Documentations

--- El-Nabaash.API/Extensions/OpenApiSwaggerExtensions.cs
+++ El-Nabaash.API/Extensions/OpenApiSwaggerExtensions.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi;
+
 namespace El_Nabaash.API.Extensions;
 
 public static class OpenApiSwaggerExtensions
@@ -5,6 +7,19 @@ public static class OpenApiSwaggerExtensions
         public static void AddOpenApiSwagger(this IServiceCollection services)
         {
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1" , new OpenApiInfo
+                {
+                    Title = "El-Nabaash API",
+                    Version = "1.0.0",
+                    Description = "An API for El-Nabaash application.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "El-Nabaash Team - Ali Ahmed El-husseiny",
+                        Email = "ali.ahmed.software.engineer@gmail.com"
+                    }
+                });
+            });
         }
 }
