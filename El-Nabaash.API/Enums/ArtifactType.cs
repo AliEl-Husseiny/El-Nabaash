f72a0264d858d8044581863074a6cdb36c4a7f9a
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-05-14 18:17:59 +0300

    Create ArtifactType Model

--- El-Nabaash.API/Enums/ArtifactType.cs
+++ El-Nabaash.API/Enums/ArtifactType.cs
@@ -2,5 +2,12 @@ namespace El_Nabaash.API.Enums;
 
 public enum ArtifactType
 {
-    
+    Weapon = 1,
+    EnergySource = 2,
+    CommunicationDevice = 3,
+    Machine = 4,
+    Tool = 5,
+    Monolith = 6,
+    Device = 7,
+    Unknown = 99
 }
