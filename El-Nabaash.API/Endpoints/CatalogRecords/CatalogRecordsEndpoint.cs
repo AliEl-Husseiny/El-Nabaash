cc3987d2db71aa22245179fa3957586983071812
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-06-15 17:20:56 +0300

    Map the Endpoint in the Route Group

--- El-Nabaash.API/Endpoints/CatalogRecords/CatalogRecordsEndpoint.cs
+++ El-Nabaash.API/Endpoints/CatalogRecords/CatalogRecordsEndpoint.cs
@@ -1,12 +1,28 @@
 using El_Nabaash.API.DTOs.CatalogRecord.Response;
 using Microsoft.AspNetCore.Http.HttpResults;
 
-namespace El_Nabaash.API.Endpoints.Artifacts;
+namespace El_Nabaash.API.Endpoints.CatalogRecords;
 
-public class CatalogRecordsEndpoint
+public static class CatalogRecordsEndpoint
 {
     
-    
+    public static IEndpointRouteBuilder MapCatalogRecordEndpoints(this IEndpointRouteBuilder route)
+    {
+        var privateGroup = route.MapGroup("/api/private/catalogrecords")
+            .RequireAuthorization()
+            .WithTags("CatalogRecords")
+            .AddEndpointFilter<ExceptionHandlingFilter>();
+
+        privateGroup.MapGet("/artifact/{artifactId:int}", GetCatalogRecordsByArtifact)
+            .WithName(nameof(GetCatalogRecordsByArtifact))
+            .WithSummary("Get Catalog Records by Artifact")
+            .WithDescription("Returns all catalog records associated with the specified artifact.")
+            .Produces<List<CatalogRecordResponseDto>>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status500InternalServerError);
+
+        return route;
+    }
     
     
     
