916d2f57556fb8eaea880dc529698931ad28a8ac
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-06-15 17:08:26 +0300

    Create the Response Model CatalogNoteResponse

--- El-Nabaash.API/DTOs/Catalog/Response/CatalogNoteResponseDto.cs
+++ El-Nabaash.API/DTOs/Catalog/Response/CatalogNoteResponseDto.cs
@@ -1,2 +1,9 @@
 namespace El_Nabaash.API.DTOs.CatalogRecord.Response;
 
+public class CatalogNoteResponseDto
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+    public string Author { get; set; } = string.Empty;
+}
