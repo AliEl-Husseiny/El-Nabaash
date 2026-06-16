ef8d317666f298b701a9d4948dc51982929d8582
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-06-16 08:53:48 +0300

    Container Supporting

--- El-Nabaash.API/Data/DataUtility.cs
+++ El-Nabaash.API/Data/DataUtility.cs
@@ -1,10 +1,25 @@
+using Npgsql;
+
 namespace El_Nabaash.API.Data;
 
 public class DataUtility
 {
     public static string GetConnectionString(IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DbConnection") ?? "";
+        var connectionString = configuration.GetConnectionString("DbConnection");
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        if (databaseUrl == null)
+        {
+            return connectionString;
+        }
+        else
+        {
+            return BuildConnectionString(databaseUrl);
+        }
+
         return connectionString;
     }
+
+    
 }
