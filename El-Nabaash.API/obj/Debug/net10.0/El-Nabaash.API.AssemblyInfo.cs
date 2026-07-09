9e56ab75a4b0beb30ff4133ee2da0ececb4060b4
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-09 11:19:26 +0300

    Clean Program Using Extinsions

--- El-Nabaash.API/obj/Debug/net10.0/El-Nabaash.API.AssemblyInfo.cs
+++ El-Nabaash.API/obj/Debug/net10.0/El-Nabaash.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1ca4ca5dc94076409ba6a68b8e69eecd008dce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553cd22e00291ef074ccc944b52eb3767e3622a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
