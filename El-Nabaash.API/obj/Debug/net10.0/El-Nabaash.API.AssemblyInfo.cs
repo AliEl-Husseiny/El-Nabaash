a8d60f307a9d194e06ca82565d5b6e778414423b
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-07-09 11:05:50 +0300

    Update Some Documentation

--- El-Nabaash.API/obj/Debug/net10.0/El-Nabaash.API.AssemblyInfo.cs
+++ El-Nabaash.API/obj/Debug/net10.0/El-Nabaash.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c060a9b81f221514e45f61ee0c085d702c7859e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1ca4ca5dc94076409ba6a68b8e69eecd008dce")]
 [assembly: System.Reflection.AssemblyProductAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("El-Nabaash.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
